8eee445eb388b5bf7f4ce049957a760a4f1af963
Buttonupd <Dankariuki0101@gmail.com>
2020-05-20 13:47:37 +0300

    re-ordered the structure of certain variables in the manage file, previous one worked too

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python
 import os
 import sys
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prof.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prof.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,6 +15,23 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+# """Django's command-line utility for administrative tasks."""
+# import os
+# import sys
 
-if __name__ == '__main__':
-    main()
+
+# def main():
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prof.settings')
+#     try:
+#         from django.core.management import execute_from_command_line
+#     except ImportError as exc:
+#         raise ImportError(
+#             "Couldn't import Django. Are you sure it's installed and "
+#             "available on your PYTHONPATH environment variable? Did you "
+#             "forget to activate a virtual environment?"
+#         ) from exc
+#     execute_from_command_line(sys.argv)
+
+
+# if __name__ == '__main__':
+#     main()
