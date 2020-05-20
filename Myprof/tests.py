eed84efe6525977fe5b1fdfd32e6efe5e259fc3f
Buttonupd <Dankariuki0101@gmail.com>
2020-05-20 13:41:02 +0300

    these are the test files, more should be added

--- Myprof/tests.py
+++ Myprof/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from __future__ import unicode_literals
+
+
+
+class SimpleTest(TestCase):
+
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
