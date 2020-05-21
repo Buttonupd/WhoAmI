7b432ec7ff8333f3f28e66d8ddd278d84460dd1c
Buttonupd <Dankariuki0101@gmail.com>
2020-05-21 23:28:24 +0300

    j

--- prof/settings.py
+++ prof/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ')tjuai#k%tgf%7b!1r-w=g9_7mr!s1(^a5n2vqke107us*2=d^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
