dbb4bc787c25b7215ed0018f038d2f180e1c0f38
Buttonupd <Dankariuki0101@gmail.com>
2020-06-12 15:39:57 +0300

    passanger

--- prof/settings.py
+++ prof/settings.py
@@ -160,8 +160,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
-EMAIL_USE_TLS = True 
-EMAIL_HOST ='smtp.gmail.com' 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST' )
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
