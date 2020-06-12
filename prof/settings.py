92b2cd13dbd2eb33730919ef54d4ff0844beead2
Buttonupd <Dankariuki0101@gmail.com>
2020-06-12 15:16:14 +0300

    master

--- prof/settings.py
+++ prof/settings.py
@@ -155,9 +155,13 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL='home'
-AUTH_PROFILE_MODULE = 'Eject.UserProfile'
+
 
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+EMAIL_USE_TLS = True 
+EMAIL_HOST ='smtp.gmail.com' 
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
